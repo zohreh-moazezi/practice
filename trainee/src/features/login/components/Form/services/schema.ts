c4360366678a8bfb063538739b0e97323d182877
zohreh moazezi <107259983+zohreh-moazezi@users.noreply.github.com>
2024-12-24 18:18:57 +0330

    feat

--- trainee/src/features/login/components/Form/services/schema.ts
+++ trainee/src/features/login/components/Form/services/schema.ts
@@ -1,9 +1,12 @@
-import {z} from "zod";
+import { z } from 'zod';
 
 const user = z.object({
-  username:z.string().min(5,{message:"Too Short"}).max(32,{message:"Too Long"}),
-  password:z.string().min(5,{message:"Too Short"}).max(32,{message:"Too Long"})
-})
-user.shape.username
-user.shape.password
-const requiredUsername=user.required();
+  username: z
+    .string()
+    .min(5, { message: 'Too Short' })
+    .max(32, { message: 'Too Long' }),
+  password: z
+    .string()
+    .min(5, { message: 'Too Short' })
+    .max(32, { message: 'Too Long' }),
+});
